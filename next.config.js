2fc799541daa0b027bebb8ec19d56bd01fc1deb7
Owais Shariff <osh.econnect@gmail>
2023-09-13 10:09:27 +0300

    Implemented CSP XSS Headers

--- next.config.js
+++ next.config.js
@@ -4,3 +4,25 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const { withSecureHeaders } = require("next-secure-headers");
+
+module.exports = withSecureHeaders({
+  contentSecurityPolicy: {
+    directives: {
+      defaultSrc: ["'self'"],
+      styleSrc: ["'self'", "'unsafe-inline'"],
+      imgSrc: ["'self'", "data:", "validator.swagger.io"],
+      scriptSrc: ["'self'", "'unsafe-inline'"],
+      connectSrc: ["'self'"],
+    },
+  },
+  forceHTTPSRedirect: [
+    true,
+    { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true },
+  ],
+  referrerPolicy: "same-origin",
+  xFrameOptions: "SAMEORIGIN",
+  xContentTypeOptions: "nosniff",
+  xXssProtection: "1; mode=block",
+});
